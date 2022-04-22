2bb6c99186b47aa11b16f26c035ad6986903f2ac
yangpengli <yangpengli@jd.com>
2022-04-22 18:25:25 +0800

    初始化spring 源码 5.1.x

---
+++ spring-hello/src/main/java/com/happyffeng/spring/demo/MyBean.java
@@ -0,0 +1,33 @@
+package com.happyffeng.spring.demo;
+
+/**
+ * TODO(用一句话描述这个类的作用)
+ *
+ * @Date: 2022/4/22 6:13 下午
+ */
+
+public class MyBean {
+	private String name = "Adom";
+
+	public MyBean(String name) {
+		this.name = name;
+	}
+
+	public MyBean() {
+	}
+
+	@Override
+	public String toString() {
+		return "MyBean{" +
+				"name='" + name + '\'' +
+				'}';
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
